e76383e2d1ae06b452143607e7b7665e8b82a835
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2026-04-17 22:43:01 -0300

    feat: add cron jobs with task scheduler

---
+++ src/modules/order/tasks/CancelPendingOrdersTask.ts
@@ -0,0 +1,18 @@
+import { OrderStatus } from "@/shared/enums/OrderStatusts";
+import { OrderRepository } from "../repositories/OrderRepository";
+
+export class CancelPendingOrdersTask {
+    constructor(private repository: OrderRepository) {}
+
+    async execute (): Promise<void> {
+        var orders = await this.repository.findPendingOrdersByMinuteAfterCreated(10); 
+
+        if(orders.length > 0) {
+             for (const order of orders) { 
+                this.repository.updateStatus(order.id, OrderStatus.CANCELLED);   
+            }
+        }
+
+        console.log(`[CRON] Cancelled ${orders.length} pending orders`);
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import { app } from "./app";
 import { AppDataSource } from "./infrastructure/database/data-source";
+import { startJobs } from "./shared/jobs";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,9 @@ const PORT = process.env.PORT || 3000;
 AppDataSource.initialize()
   .then(() => {
     console.log("Database is connected");
+    
+    startJobs();
+
     app.listen(PORT, () => {
       console.log(`Server is running on PORT: ${PORT}`);
     });
