a970bbb5cee314e3cce25b53f0a7182b7bf6951c
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2026-04-22 23:32:36 -0300

    feat: add pino setting loggers

---
+++ src/config/logger.ts
@@ -0,0 +1,34 @@
+import pino from "pino";
+
+const isEnvProduction = process.env.NODE_ENV === "production";
+
+export const logger = pino({
+  level: process.env.LOG_LEVEL || "info",
+  timestamp: !isEnvProduction
+    ? () =>
+        `,"time":"${new Date().toLocaleString("pt-BR", {
+          timeZone: "America/Sao_Paulo",
+        })}"`
+    : pino.stdTimeFunctions.isoTime,
+  transport: !isEnvProduction
+    ? {
+        target: "pino-pretty",
+        options: {
+          colorize: true,
+          translateTime: "HH:MM:ss",
+          ignore: "pid,hostname",
+        },
+      }
+    : undefined,
+  serializers: {
+    req: (req) => ({
+      method: req.method,
+      url: req.url,
+      id: req.id,
+    }),
+    res: (res) => ({
+      statusCode: res.statusCode,
+    }),
+    err: pino.stdSerializers.err,
+  },
+});
