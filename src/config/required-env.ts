ae45a9473694059eeb0ec61a094a405d374c6a9d
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2026-04-29 22:03:07 -0300

    chore: change file is not setting

--- src/config/required-env.ts
+++
@@ -1,9 +0,0 @@
-export function required(name: string): string {
-  const value = process?.env[name]
-
-  if (!value) {
-    throw new Error(`Environment variable is required: ${name}`)
-  }
-
-  return value
-}
